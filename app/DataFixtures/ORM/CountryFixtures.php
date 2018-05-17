84ddab6c6f540507e2eb5ad59c267e4cddd45a11
StevenGrl <steven.grillon.45@gmail.com>
2018-05-17 19:14:21 +0200

    fixture team

--- app/DataFixtures/ORM/CountryFixtures.php
+++ app/DataFixtures/ORM/CountryFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use AppBundle\Entity\Country;
+use AppBundle\Entity\Team;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -15,6 +16,13 @@ class CountryFixtures extends Fixture
             $country->setCode($data['code']);
             $country->setName($data['name']);
             $manager->persist($country);
+
+            if (isset($data['name_team'])) {
+                $team = new Team();
+                $team->setName($data['name_team']);
+                $team->setCountry($country);
+                $manager->persist($team);
+            }
         }
         $manager->flush();
     }
@@ -24,15 +32,103 @@ class CountryFixtures extends Fixture
         return [
             [
                 'code' => 'PT',
-                'name' => 'Portugal'
+                'name' => 'Portugal',
+                'name_team' => 'Pony Hairy'
             ],
             [
                 'code' => 'AR',
-                'name' => 'Argentine'
+                'name' => 'Argentine',
+                'name_team' => 'Pony Shiny'
             ],
             [
                 'code' => 'AT',
-                'name' => 'Austria'
+                'name' => 'Austria',
+                'name_team' => 'Pony Fairy'
+            ],
+            [
+                'code' => 'AU',
+                'name' => 'Australia',
+                'name_team' => 'My KangooPony'
+            ],
+            [
+                'code' => 'BD',
+                'name' => 'Bangladesh'
+            ],
+            [
+                'code' => 'BE',
+                'name' => 'Belgium',
+                'name_team' => 'Pony Silly'
+            ],
+            [
+                'code' => 'BL',
+                'name' => 'Saint Barthélemy',
+                'name_team' => 'My Quiet Pony'
+            ],
+            [
+                'code' => 'BR',
+                'name' => 'Brazil',
+                'name_team' => 'My Soccer Player Pony'
+            ],
+            [
+                'code' => 'CA',
+                'name' => 'Canada',
+                'name_team' => 'Inapprehensible Pony'
+            ],
+            [
+                'code' => 'CH',
+                'name' => 'Switzerland',
+                'name_team' => 'Pony Wealthy'
+            ],
+            [
+                'code' => 'CN',
+                'name' => 'China',
+                'name_team' => 'My Baby Pony'
+            ],
+            [
+                'code' => 'DE',
+                'name' => 'Germany',
+                'name_team' => 'My Arryan Pony'
+            ],
+            [
+                'code' => 'DK',
+                'name' => 'Denmark',
+                'name_team' => 'My Stoned Pony'
+            ],
+            [
+                'code' => 'ES',
+                'name' => 'Spain',
+                'name_team' => 'Bella Ciao Pony'
+            ],
+            [
+                'code' => 'FR',
+                'name' => 'France',
+                'name_team' => 'Mon petit poney'
+            ],
+            [
+                'code' => 'LU',
+                'name' => 'Luxembourg'
+            ],
+            [
+                'code' => 'MX',
+                'name' => 'Mexico',
+                'name_team' => 'My Pony Walking On The Wall'
+            ],
+            [
+                'code' => 'PH',
+                'name' => 'Philippines'
+            ],
+            [
+                'code' => 'PL',
+                'name' => 'Poland',
+                'name_team' => 'My Vodka and My Pony'
+            ],
+            [
+                'code' => 'PR',
+                'name' => 'Puerto Rico'
+            ],
+            [
+                'code' => 'PY',
+                'name' => 'Paraguay'
             ],
         ];
     }
